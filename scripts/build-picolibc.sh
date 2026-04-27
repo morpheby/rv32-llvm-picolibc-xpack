9ba2b6e01242e367eeca4027a52950c50100681d
Ilya Mikhaltsou <morpheby@gmail.com>
2026-04-27 19:13:20 +0300

    Adjust definition of mb-capable

--- scripts/build-picolibc.sh
+++ scripts/build-picolibc.sh
@@ -30,19 +30,12 @@ for s in "${PICOLIBC_CROSS_FILES_DIR}"/*.txt ; do
   b="$(basename "$s")"
   b="${b%.*}"
 
-  if [[ "${b}" == *_minimal ]]; then
-    MB_CAPABLE=false
-  else
-    MB_CAPABLE=true
-  fi
-
   meson setup "build-${b}/"       \
     --cross-file="${s}"           \
     --prefix="${PICOLIBC_PREFIX}" \
     -Dspecsdir=none               \
     -Dincludedir=include          \
     -Dlibdir=lib                  \
-    -Dmb-capable=${MB_CAPABLE}    \
     -Dmultilib=false              \
     --buildtype=minsize           \
     --reconfigure
